e815320ac1d5b796dd6c89d6d0806943c37451e1
mumbi65 <mumbic65@gmail.com>
2024-05-21 21:27:02 +0300

    updated changes

---
+++ js/revision.js
@@ -0,0 +1,27 @@
+{
+    let age = 30
+        if (age <= 3){
+            console.log("child should be at home")
+        }
+        else if(age <= 6){
+            console.log("should be kindergarten")
+        }
+        else if(age <= 10){
+            console.log("Lower Primary")
+        }
+        else if(age <= 14){
+            console.log("Upper Primary")
+        }
+        else if(age <= 18){
+            console.log("High School")
+        }
+        else if(age <= 24){
+            console.log("Campus")
+        }
+        else if(age <= 60){
+            console.log("Employed")
+        }
+        else{
+            console.log("Retired")
+        }
+}
